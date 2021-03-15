15524c51d71a7f41c0468c78fb6de4deffb63713
vishwa795 <visvig66@gmail.com>
2021-03-15 14:56:00 +0530

    Todos

---
+++ src/Components/complaintListComponent.js
@@ -0,0 +1,4 @@
+import React, {Component} from 'react';
+//Like Quora's homepage
+//cardComponent
+//modal with title and desc - https://reactstrap.github.io/components/modals/
--- src/shared/exampleData.js
+++ src/shared/exampleData.js
@@ -26,3 +26,24 @@ export const stateData ={
         res_today:"14",
     }
 }
+
+export const complaintsData = [
+    {
+        id:1,
+        title:"Roads have a problem",
+        description:"The roads in my area, that is, Kaggadasapura have been very bad. Please try to correct it soon",
+        upvotes:278
+    },
+    {
+        id:2,
+        title:"Water Scarcity",
+        description:"Water has become a lot scarce this year. Kindly help those in need with adequate supply of water. Many people in my area are sufferring withou water for basic neccesities",
+        upvotes:37
+    },
+    {
+        id:3,
+        title:"Power Cuts",
+        description:"There are regular power cuts in my area and for people like me working from home, this becomes a huge problem as our offices are working online and without regular electricity it causes us inconvinence for completing our daily tasks. Kindly ensure to provide continious supply of electricty!",
+        upvotes:567
+    }
+]
