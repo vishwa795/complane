6b6f53c54aadce98ae2f3407d5b3ae489a5a63d3
roy-a2yush <roy.a2yush@gmail.com>
2021-06-12 13:19:30 +0530

    skeleton for complaints made

--- src/API_calls/complaints.js
+++ src/API_calls/complaints.js
@@ -1,19 +1,54 @@
-function getAllComplaints() {
+function getAllComplaints(state, sort) {
     fetch(url + '/complaints')
       .then(res => res.json())
 }
 
-function getAllComplaintsForUser(uid) {
+function getAllComplaintsForUser(uid, sort) {
     fetch(url + '/complaints/user/'+uid)
       .then(res => res.json())
 }
 
-function getAllActiveComplaints() {
-    fetch(url + '/active')
+function getAllActiveComplaints(state, sort) {
+    fetch(url + '/complaints/active')
       .then(res => res.json())
 }
 
-function getAllComplaintsForUser(uid) {
-    fetch(url + '/complaints/user/'+uid)
+function getAllNewComplaints(state, sort) {
+    fetch(url + '/complaints/new')
+      .then(res => res.json())
+}
+
+function getAllResolvedComplaints(state, sort) {
+    fetch(url + '/complaints/resolved')
+      .then(res => res.json())
+}
+
+function getAllNewResolvedComplaints(state) {
+    fetch(url + '/complaints/resolvedToday')
+      .then(res => res.json())
+}
+
+function getActiveComplaintCount(state) {
+    fetch(url + '/complaints/count/active')
+      .then(res => res.json())
+}
+
+function getResolvedComplaintCount(state) {
+    fetch(url + '/complaints/count/resolved')
+      .then(res => res.json())
+}
+
+function getActiveTodayComplaintCount(state) {
+    fetch(url + '/complaints/count/active/today')
+      .then(res => res.json())
+}
+
+function getResolvedTodayComplaintCount(state) {
+    fetch(url + '/complaints/count/resolved/today')
+      .then(res => res.json())
+}
+
+function postComplaintRegister() {
+    fetch(url + '/complaint/register')
       .then(res => res.json())
 }
