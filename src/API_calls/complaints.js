5d8e9209dd82351fd8cbffc7059fe3f0ad9c14c0
shreyas2499 <shreyas2499@gmail.com>
2021-06-13 14:42:37 +0530

    Api calls

--- src/API_calls/complaints.js
+++ src/API_calls/complaints.js
@@ -1,54 +1,96 @@
-function getAllComplaints(state, sort) {
-    fetch(url + '/complaints')
+/*
+List of constants in this script : 
+
+complaints
+userComplaints
+activeComplaints
+newComplaints
+resolvedComplaints
+newResolvedComplaints
+activeComplaintsCount
+resolvedComplaintsCount
+activeTodayComplaintCount
+resolvedTodayComplaintCount
+complaintRegister
+*/
+
+async function getAllComplaints(state, sort) {
+  const complaints = await  fetch(url + '/complaints',
+          state=state,
+          sort=sort,
+    )
+      .then(res => res.json())
+     //Console log complaints here will print complaints after they are fetched from db
+}
+
+async function getAllComplaintsForUser(uid, sort) {
+    const userComplaints = await fetch(url + '/complaints/user/'+uid,
+    sort=sort,
+  )
       .then(res => res.json())
 }
 
-function getAllComplaintsForUser(uid, sort) {
-    fetch(url + '/complaints/user/'+uid)
+
+async function getAllActiveComplaints(state, sort) {
+    const activeComplaints = await fetch(url + '/complaints/active',
+    state=state,
+    sort=sort,
+    )
       .then(res => res.json())
 }
 
-function getAllActiveComplaints(state, sort) {
-    fetch(url + '/complaints/active')
+async function getAllNewComplaints(state, sort) {
+    const newComplaints = await fetch(url + '/complaints/new',
+          state=state,
+          sort=sort,
+    )
+    .then(res => res.json())
+}
+
+async function getAllResolvedComplaints(state, sort) {
+    const resolvedComplaints = await fetch(url + '/complaints/resolved',
+    state=state,
+    sort=sort,
+    )
       .then(res => res.json())
 }
 
-function getAllNewComplaints(state, sort) {
-    fetch(url + '/complaints/new')
+async function getAllNewResolvedComplaints(state) {
+    const newResolvedComplaints = await fetch(url + '/complaints/resolvedToday',
+    state=state,
+    )
       .then(res => res.json())
 }
 
-function getAllResolvedComplaints(state, sort) {
-    fetch(url + '/complaints/resolved')
+async function getActiveComplaintCount(state) {
+    const activeComplaintsCount = await fetch(url + '/complaints/count/active',
+    state=state,
+    )
       .then(res => res.json())
 }
 
-function getAllNewResolvedComplaints(state) {
-    fetch(url + '/complaints/resolvedToday')
+async function getResolvedComplaintCount(state) {
+    const resolvedComplaintsCount = await fetch(url + '/complaints/count/resolved',
+    state=state,
+    )
       .then(res => res.json())
 }
 
-function getActiveComplaintCount(state) {
-    fetch(url + '/complaints/count/active')
+async function getActiveTodayComplaintCount(state) {
+    const activeTodayComplaintCount = await fetch(url + '/complaints/count/active/today',
+    state=state,
+    )
       .then(res => res.json())
 }
 
-function getResolvedComplaintCount(state) {
-    fetch(url + '/complaints/count/resolved')
+async function getResolvedTodayComplaintCount(state) {
+    const resolvedTodayComplaintCount = await fetch(url + '/complaints/count/resolved/today',
+    state=state,
+    )
       .then(res => res.json())
 }
 
-function getActiveTodayComplaintCount(state) {
-    fetch(url + '/complaints/count/active/today')
-      .then(res => res.json())
-}
-
-function getResolvedTodayComplaintCount(state) {
-    fetch(url + '/complaints/count/resolved/today')
-      .then(res => res.json())
-}
-
-function postComplaintRegister() {
-    fetch(url + '/complaint/register')
+async function postComplaintRegister() {
+    const complaintRegister = await fetch(url + '/complaint/register')
       .then(res => res.json())
 }
